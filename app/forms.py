748853423564b8c3bde84f981def1d19d13fea8d
lukeskyan <yanlucasdejesus@gmail.com>
2021-10-26 11:32:00 -0300

    Primeiro commit

---
+++ app/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms.fields.html5 import EmailField
+from wtforms.fields import PasswordField, BooleanField, SubmitField
+
+
+
+class LoginForm(FlaskForm):
+    email = EmailField("Email")
+    password = PasswordField("Senha")
+    remember = BooleanField("Permanecer conectado")
+    submit = SubmitField("Logar")
